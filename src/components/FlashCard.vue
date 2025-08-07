4fb80c5c020d62cba51bd49f8749e7f254ffa859
aneylon <arlen.m.neylon@gmail.com>
2025-08-06 18:00:12 -0600

    start card setup.

---
+++ src/components/FlashCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+let showCardBack = ref(true);
+
+const toggleCardBack = () => {
+  console.log("toggle");
+  showCardBack = !showCardBack;
+  console.log(showCardBack);
+};
+
+function knowCard() {
+  console.log("Know Card");
+}
+function dontKnowCard() {
+  console.log("Dont Know Card");
+}
+function notSureCard() {
+  console.log("Not Sure Card");
+}
+</script>
+
+<template>
+  <div>
+    <div @click="toggleCardBack">question</div>
+    <div id="cardBack" v-show="showCardBack">
+      <div>answer</div>
+      <div>explain</div>
+    </div>
+    <div id="buttons">
+      <button @click="knowCard">Know</button>
+      <button @click="notSureCard">Not sure</button>
+      <button @click="dontKnowCard">Don't Know</button>
+    </div>
+  </div>
+</template>
---
+++ src/components/Test.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><h1>Test</h1></div>
+</template>
