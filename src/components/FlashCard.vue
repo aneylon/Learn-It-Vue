42f9169d511ad69cd0419a855db6b89c13ee08ca
aneylon <arlen.m.neylon@gmail.com>
2025-08-07 19:36:13 -0600

    Update flashcard component.

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -1,35 +1,60 @@
-<script setup>
-import { ref } from "vue";
-let showCardBack = ref(true);
-
-const toggleCardBack = () => {
-  console.log("toggle");
-  showCardBack = !showCardBack;
-  console.log(showCardBack);
+<script>
+export default {
+  data() {
+    return {
+      showCardBack: false,
+      currentCardIndex: 0,
+      currentCard: {
+        question: "This is a question",
+        answer: "This is an answer",
+        explanation: "This is an explanation",
+      },
+      cards: [
+        { question: "One", answer: "One", explanation: "One" },
+        { question: "Two", answer: "Two", explanation: "Two" },
+        { question: "Three", answer: "Three", explanation: "Three" },
+        { question: "Four", answer: "Four", explanation: "Four" },
+      ],
+    };
+  },
+  methods: {
+    toggleCardBack() {
+      this.showCardBack = !this.showCardBack;
+    },
+    nextCard() {
+      console.log("Go to the next card");
+      this.currentCardIndex += 1;
+    },
+    shuffleCards() {
+      console.log("shuffle cards");
+    },
+    cardResponse(response) {
+      console.log(response, "this card");
+      // todo : save user response
+      // push card to relevant array
+      // decide what to do next
+      console.log(this.currentCardIndex, this.cards.length);
+      if (this.currentCardIndex === this.cards.length) {
+        this.shuffleCards();
+      } else {
+        this.nextCard();
+      }
+    },
+  },
 };
-
-function knowCard() {
-  console.log("Know Card");
-}
-function dontKnowCard() {
-  console.log("Dont Know Card");
-}
-function notSureCard() {
-  console.log("Not Sure Card");
-}
 </script>
 
 <template>
   <div>
-    <div @click="toggleCardBack">question</div>
+    <div @click="toggleCardBack">{{ currentCard.question }}</div>
     <div id="cardBack" v-show="showCardBack">
-      <div>answer</div>
-      <div>explain</div>
+      <div>{{ currentCard.answer }}</div>
+      <div>{{ currentCard.explanation }}</div>
     </div>
     <div id="buttons">
-      <button @click="knowCard">Know</button>
-      <button @click="notSureCard">Not sure</button>
-      <button @click="dontKnowCard">Don't Know</button>
+      <button @click="cardResponse('Know')">Know</button>
+      <button @click="cardResponse('NotSure')">Not sure</button>
+      <button @click="cardResponse('DontKnow')">Don't Know</button>
     </div>
   </div>
 </template>
